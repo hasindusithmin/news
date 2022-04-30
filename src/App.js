ca28342c33291c5c40477e9430467b1db0595cc1
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-30 16:53:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,100 @@
 
 import './App.css';
 
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [first, setFirst] = useState(null)
+  const [last, setLast] = useState(null)
+  const [uname, setUname] = useState(null)
+  const [profile, setProfile] = useState(null)
+  const [loginAt, setLoginAt] = useState(null)
+  const [auth, setAuth] = useState(false)
+  useEffect(() => {
+    if (sessionStorage.getItem('hash')) setAuth(true)
+    setFirst(sessionStorage.getItem('firstname'))
+    setLast(sessionStorage.getItem('lastname'))
+    setUname(sessionStorage.getItem('username'))
+    setProfile(sessionStorage.getItem('profile'))
+    setLoginAt(sessionStorage.getItem('loginat'))
+  }, [])
+
+
   return (
-    <div>Hello World</div>
+    <>
+      {auth && 
+      <div className="container bootstrap snippets bootdey">
+        <div className="panel-body inf-content">
+          <div className="row">
+            <div className="col-md-4">
+              <img alt="Add Profile Image to telegram" style={{ widows: '600px' }} title="" className="img-circle img-thumbnail isTooltip" src={profile !== '' ? profile : ''} data-original-title="Usuario" />
+              <ul title="Ratings" className="list-inline ratings text-center">
+                <li><a href="#"><span className="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span className="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span className="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span className="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span className="glyphicon glyphicon-star"></span></a></li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <strong>Information</strong><br />
+              <div className="table-responsive">
+                <table className="table table-user-information">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-user  text-primary"></span>
+                          Name
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        {first}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-cloud text-primary"></span>
+                          Lastname
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        {last}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-bookmark text-primary"></span>
+                          Username
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        {uname}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>
+                          <span className="glyphicon glyphicon-calendar text-primary"></span>
+                          LoginAt
+                        </strong>
+                      </td>
+                      <td className="text-primary">
+                        {loginAt}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>}
+      {!auth && <div style={{margin:'20px',color:'red'}}>please login via telegram</div>}
+    </>
   );
 }
 
